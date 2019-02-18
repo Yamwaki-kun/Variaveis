152700c41031af25c5de9642dd675941f2a2740b
Arnaldo Victor Yamawaki Alves <43183787+Yamwaki-kun@users.noreply.github.com>
2019-02-18 16:35:19 -0300

    Add files via upload

--- desafio.sh
+++ desafio.sh
@@ -1,20 +1,23 @@
 #!/bin/bash 
-HORA=$(date +%r)
+HORA=$(date +%H)
+HORAM=$(date +%M)
+HORAS=$(date +%S)
 DIASEM=$(date +%a)
 DIA=$(date +%d)
 MES=$(date +%b)
 ANO=$(date +%Y)
+TESTE01=$(test -w /root ; echo $?)
 #1
 echo "Bem vindo Hoje é $DIA de $MES de $ANO"
 #2
-echo "São $HORA"
+echo "São $HORA:$HORAM:$HORAS"
 #3
 
 if [ -e "/tmp" ]
 	then
 		echo " O diretorio /tmp existe"
 	else
-		echo " O diretorio não existe"
+		echo " O diretorio /tmp não existe"
 fi
 #4
 
@@ -22,7 +25,7 @@ if [ -e "/tmp/arnaldo_victor" ]
 	then
 		echo " O diretorio /tmp/arnaldo_victor existe"
 	else
-		echo " O diretorio não exite agora o criaremos"
+		echo " O diretorio /tmp/arnaldo_victor não exite agora o criaremos"
 	mkdir /tmp/arnaldo_victor
 		echo "Pronto"
 fi
@@ -38,3 +41,9 @@ if [ -e /tmp/arnaldo_victor/arnaldo.txt ]
 		echo "Pronto"
 fi
 
+#6
+case $TESTE01 in
+	0) echo "resultado do comando case é...: 0- verdadeiro";;
+	1) echo "resultado do comando case é...: 1- falso";;
+	*) echo "resultado do comando case é...: não indentificado"
+esac
