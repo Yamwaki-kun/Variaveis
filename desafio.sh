ee8eda9a230cc0f6cca23dd883dfc22bf84ed8d1
Arnaldo Victor Yamawaki Alves <43183787+Yamwaki-kun@users.noreply.github.com>
2019-02-19 01:32:11 -0300

    Add files via upload

--- desafio.sh
+++ desafio.sh
@@ -7,13 +7,16 @@ DIA=$(date +%d)
 MES=$(date +%b)
 ANO=$(date +%Y)
 TESTE01=$(test -w /root ; echo $?)
+TMP=/tmp
+DIR=/tmp/arnaldo_victor/
+ARQ=arnaldo.txt
 #1
 echo "Bem vindo Hoje é $DIA de $MES de $ANO"
 #2
 echo "São $HORA:$HORAM:$HORAS"
 #3
 
-if [ -e "/tmp" ]
+if [ -e "$TMP" ]
 	then
 		echo " O diretorio /tmp existe"
 	else
@@ -21,23 +24,23 @@ if [ -e "/tmp" ]
 fi
 #4
 
-if [ -e "/tmp/arnaldo_victor" ]
+if [ -e "$DIR" ];
 	then
 		echo " O diretorio /tmp/arnaldo_victor existe"
 	else
 		echo " O diretorio /tmp/arnaldo_victor não exite agora o criaremos"
-	mkdir /tmp/arnaldo_victor
+	mkdir $DIR
 		echo "Pronto"
 fi
 #5
 
-if [ -e /tmp/arnaldo_victor/arnaldo.txt ]
+if [ -e "$DIR$ARQ" ];
    then 
       echo "Arquivo /tmp/arnaldo_victor/arnaldo.txt existe."
    
    else 
       echo "Arquivo /tmp/arnaldo_victor/arnaldo.txt não existe. Agóra será criado."
-	touch /tmp/arnaldo_victor/arnaldo.txt
+	touch $DIR$ARQ
 		echo "Pronto"
 fi
 
@@ -47,3 +50,43 @@ case $TESTE01 in
 	1) echo "resultado do comando case é...: 1- falso";;
 	*) echo "resultado do comando case é...: não indentificado"
 esac
+cat << "EOF"
+           ,--"""",--.__,---[],-------._         
+       ,"   __,'            \         \--""""""==;-
+     ," _,-"  "/---.___     \       ___\   ,-'',"
+    /,-'      / ;. ,.--'-.__\  _,-"" ,| `,'   /
+   /``""""-._/,-|:\       []\,' ```-/:;-. `. /
+             `  ;:::      ||       /:,;  `-.\
+                =.,'__,---||-.____',.=
+                =(:\_     ||__    ):)=
+               ,"::::`----||::`--':::"._
+             ,':::::::::::||::::::::::::'.
+    .__     ;:::.-.:::::__||___:::::.-.:::\     __,
+       """-;:::( O )::::>_|| _<::::( O )::::-"""
+   =======;:::::`-`:::::::||':::::::`-`:::::\=======
+    ,--"";:::_____________||______________::::""----.          , ,
+         ; ::`._(    |    |||     |   )_,'::::\_,,,,,,,,,,____/,'_,
+       ,;    :::`--._|____[]|_____|_.-'::::::::::::::::::::::::);_
+      ;/ /      :::::::::,||,:::::::::::::::::::::::::::::::::::/
+     /; ``''''----------/,'/,__,,,,,____:::::::::::::::::::::,"
+     ;/                :);/|_;| ,--.. . ```-.:::::::::::::_,"
+    /;                :::):__,'//""\\. ,--.. \:::,:::::_,"
+   ;/              :::::/ . . . . . . //""\\. \::":__,"
+   ;/          :::::::,' . . . . . . . . . . .:`::\
+   ';      :::::::__,'. ,--.. . .,--. . . . . .:`::`
+   ';   __,..--'''-. . //""\\. .//""\\ . ,--.. :`:::`
+   ;    /  \\ .//""\\ . . . . . . . . . //""\\. :`::`
+   ;   /       . . . . . . . . . . . . . . . . .:`::`
+   ;   (          . . . . . . . . . . . . . . . ;:::`
+   ,:  ;,            . . . . . . . . . . . . . ;':::`
+   ,:  ;,             . . . . . . . . . . . . .;`:::
+   ,:   ;,             . . . . . . . . . . . . ;`::;`
+    ,:  ;             . . . . . . . . . . . . ;':::;`
+     :   ;             . . . . . . . . . . . ,':::;
+      :   '.          . . . . . . . .. . . .,':::;`
+       :    `.       . . . . . . . . . . . ;::::;`
+        '.    `-.   . . . . . . . . . . ,-'::::;
+          `:_    ``--..___________..--'':::::;'`
+             `._::,.:,.:,:_ctr_:,:,.::,.:_;'`
+________________`"\/"\/\/'""""`\/"\/""\/"____________________________
+EOF
